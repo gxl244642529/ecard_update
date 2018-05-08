44d39db44fa3452a6554c880a421dd3b8ee3a8e4
gong <244642529@qq.com>
2018-05-08 18:06:22 +0800

    理财

--- app/components/Licai/Recommend.js
+++ app/components/Licai/Recommend.js
@@ -10,7 +10,8 @@ import {
   StyleSheet,
   Text,
   View ,
-  ScrollView
+  ScrollView,
+  Image
 } from 'react-native';
 import LargeTitle from '../../widget/LargeTitle'
 
@@ -22,6 +23,19 @@ export default class Recommend extends Component<Props> {
     return (
      <ScrollView style={{flex:1,backgroundColor:'#f8f9fa'}}>
         <View style={{backgroundColor:"red",margin:15,height:150,borderRadius:5}}></View>
+        <View style={{paddingTop:10,paddingLeft:15}}>
+          <Text style={{fontSize:10,color:'#808080'}}>精选理财</Text>
+          <ScrollView horizontal={true}>
+            <Image style={{width:170,height:107}} source={require('./images/select.png')} resizeMode="contain"/>
+            <Image style={{width:170,height:107}} source={require('./images/select.png')} resizeMode="contain"/>
+          </ScrollView>
+        </View>
+        <View style={{paddingTop:10,paddingLeft:15}}>
+          <View>
+            <Text style={{fontSize:10,color:'#808080'}}>浙金理财</Text>
+            <Image style={{height:90,width:345}} resizeMode="stretch" source={require('./images/pro.png')}/>
+          </View>
+        </View>
       </ScrollView>
     );
   }
