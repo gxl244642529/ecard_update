b5f6f3cf424e352b4aa30c66a7acba8d2906f288
gong <244642529@qq.com>
2018-05-14 08:44:12 +0800

    Revert "首页动态配置"
    This reverts commit 61a8491714871b453cd0ce93f753c3bbe87bb73b.

--- app/components/Home/index.js
+++ app/components/Home/index.js
@@ -12,7 +12,6 @@ import {
   ImageBackground,
   RefreshControl
 } from 'react-native';
-const SysModule = require('react-native').NativeModules.SysModule;
 import Alert from '../../lib/Alert'
 import Sms from '../../lib/Sms'
 import Share from '../../lib/Share'
@@ -20,21 +19,40 @@ import Share from '../../lib/Share'
 const WIDTH = Dimensions.get('window').width;
 import Carousel, { ParallaxImage } from 'react-native-snap-carousel';
 import layout from '../../styles/Layout'
-import AdvView from '../../widget/AdvView'
+import AdvView from '../../../src/widget/AdvView'
 import HomeStyle from './index.style.js'
 import NavHelper from '../../lib/NavHelper'
 import Api from '../../../lib/network/Api'
-import LoginUtil from '../../../lib/LoginUtil'
 import MainButton from './MainButton'
 import MenuButton from './MenuButton'
 import ActionBar from './ActionBar'
-import Header from './Header'
-import Cardousel from './Cardousel'
-import GlobalStyle from '../GlobalStyle'
-import Modules from '../Modules'
-// import {onRequireLoginPress} from '../../../lib/LoginUtil'
-import {isLogin,loginSuccess,onRequireLoginPress} from '../../../lib/LoginUtil'
+import {onRequireLoginPress} from '../../../lib/LoginUtil'
 const refreshColors = ['#ff0000','#00ff00','#0000ff','#3ad564'];
+
+class Header extends Component{
+
+  render(){
+    return(
+      <View style={{backgroundColor:'#fff'}}>
+        <View style={{height:50,alignItems:'center',justifyContent:'space-between',flexDirection:'row',marginLeft:15,marginRight:15,marginTop:12}}>
+          <TouchableOpacity style={{flexDirection:'row',alignItems:'center'}} onPress={this.props.onPress}>
+            <Image source={require('./images/head.png')} style={{width:30,height:30,marginLeft:15,borderRadius:5}} />
+            <Text style={{paddingLeft:14.5}}>184*****681</Text>
+          </TouchableOpacity>
+          <View style={{flexDirection:'row',alignItems:'center'}}>
+            <TouchableOpacity>
+              <Image source={require('./images/onli_server.png')} style={{width:20,height:22,margin:14.5}} resizeMode="contain"/>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Image source={require('./images/notice.png')} style={{width:20,height:22,marginRight:14.5}} resizeMode="contain"/>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    )
+  }
+}
+
 
 export default class  Home extends Component{
   static navigationOptions = {
@@ -42,35 +60,56 @@ export default class  Home extends Component{
   }
   constructor(props) {
     super(props);
+    let images = [
+      {thumbnail:require('./images/card_d1.png')} ,
+      {thumbnail:require('./images/card_d1.png')} ,
+      {thumbnail:require('./images/card_d1.png')} ,
+    ];
+
+    let advdata = [
+      {img:'http://110.80.22.108:8887/uploads/2018_04_26/8e7afcb27697fe7458fc3092a5842070.jpg',url:'https://www.baidu.com/',title:"测试"},
+      {img:'http://110.80.22.108:8887/uploads/2018_04_26/8e7afcb27697fe7458fc3092a5842070.jpg',url:'https://www.baidu.com/',title:"测试"}
+    ]
+
     this.state={
+      entries:images,
       isRefreshing:true,
     }
+
   }
   componentDidMount(){
-    SysModule.onStartup();
-    console.log(isLogin());
-      // this._onRefresh();
+      this._onRefresh();
   }
-  // _onRefresh=()=>{
-  //   let data = {channel:'app',gCodes:"main,sub,action"}
-  //   Api.detail(this,{
-  //    api:'acc_base/modules',
-  //    data:data,
-  //    success:this._loadComplete,
-  //    error:this._loadComplete,
-  //    message:this._loadComplete
-  //  });
-  // }
-  _loadComplete=(result)=>{
-    this.setState(result);
+  _onRefresh=()=>{
+    let data = {channel:'app',gCodes:"main,sub,action"}
+    Api.detail(this,{
+     api:'acc_base/modules',
+     data:data,
+     success:this._loadComplete,
+     error:this._loadComplete,
+     message:this._loadComplete
+   });
+  }
+  _loadComplete=()=>{
    this.setState({isRefreshing:false});
  }
   _onPress=()=>{
     console.log("测试")
     NavHelper.push('Test1');
   }
-_onRefresh=()=>{
-  this.ref.MODULES._loadData();
+  _renderCardItem ({item, index}, parallaxProps) {
+    return (
+        <TouchableOpacity >
+            <ParallaxImage
+                source={ item.thumbnail}
+                containerStyle={{height:188}}
+                style={{height:350,width:188,resizeMode:'contain'}}
+                parallaxFactor={0.4}
+                {...parallaxProps}
+            />
+
+        </TouchableOpacity>
+    );
 }
 renderRefresh=()=>{
   return (
@@ -85,18 +124,24 @@ renderRefresh=()=>{
   render() {
      const refresh = this.renderRefresh();
      return (
-       <Modules gCodes={"main,sub,action"} _loadComplete={this._loadComplete}  ref="MODULES">
-         <View style={GlobalStyle.container}>
-            <Header onPress={()=>{this._onPress()}}/>
-            <ScrollView   refreshControl={refresh}>
-              <Cardousel api="acc_ecard/homeList"/>
-              <MainButton data={this.state.main}/>
-              <MenuButton data={this.state.sub}/>
-              <ActionBar  data={this.state.action}/>
-              <AdvView id="main"/>
-            </ScrollView>
-         </View>
-       </Modules>
+       <View style={{flex:1,backgroundColor:'#f3f4f5'}}>
+
+          <Header onPress={()=>{this._onPress()}}/>
+
+          <ScrollView style={{backgroundColor:'#fff',paddingTop:6}}   refreshControl={refresh}>
+            <Carousel
+              data={this.state.entries}
+              renderItem={this._renderCardItem}
+              sliderWidth={WIDTH}
+              itemWidth={310}
+              hasParallaxImages={true}
+            />
+            <MainButton data={this.state.main}/>
+            <MenuButton data={this.state.sub}/>
+            <ActionBar  data={this.state.action}/>
+            <AdvView ref="ADV" data={this.state.advdata} width={300} height={300}/>
+          </ScrollView>
+       </View>
      );
    }
 }
--- app/widget/PhoneUtil.js
+++
@@ -1,9 +0,0 @@
-
-
-
-export  function encodePhone(tel){
-    var reg = /^(\d{3})\d{4}(\d{4})$/;
-    tel = tel.replace(reg, "$1****$2");
-    // console.log(tel);
-  	return tel;
-	}
